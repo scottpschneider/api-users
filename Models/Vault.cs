19ea05c782cc6847ecd456e9018f97e551ccc2fa
Scott <schneider_scott@live.com>
2018-07-10 16:27:16 -0600

    vault tested good

--- Models/Vault.cs
+++ Models/Vault.cs
@@ -3,7 +3,7 @@ namespace API_Users.Models{
 public class Vault
 {
 public int Id { get; set; }
-public string Title { get; set; }
+public string Name { get; set; }
 public string Body { get; set; }
 public string AuthorId { get; set; }
 
