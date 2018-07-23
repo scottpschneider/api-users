5174e85c16b1cd52e02eb29a485cfb0db7c5a5e5
Scott <schneider_scott@live.com>
2018-07-23 15:40:32 -0600

    userViewKeep.imgURL tried again

--- wwwsrc/src/components/Keeps.vue
+++ wwwsrc/src/components/Keeps.vue
@@ -18,7 +18,7 @@
         </div>
       </div>
       <div class="modal" id="keepMyViewKeep" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
+        <div v-for= "(keep, index) in keeps" class="modal-dialog" :key="index" role="document">
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title">{{userViewKeep.name}}</h5>
